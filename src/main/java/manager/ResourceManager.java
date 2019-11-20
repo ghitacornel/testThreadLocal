467f688e955b8c9d27714c48070387fd9d5da227
ghitacornel <ghita.cornel@gmail.com>
2019-11-20 15:44:11 +0200

    cleanup

--- src/main/java/manager/ResourceManager.java
+++ src/main/java/manager/ResourceManager.java
@@ -3,11 +3,14 @@ package manager;
 import resource.Resource;
 
 /**
- * THREAD oriented specific resource MANAGER<br>
- * It is usually designed as a SINGLETON
+ * THREAD oriented specific resource MANAGER
+ * This MANAGER is usually designed as a SINGLETON
  */
 public class ResourceManager extends ThreadLocal<Resource> {
 
+    /*
+     * Returns the current thread's "initial value" for this thread-local variable.
+     */
     @Override
     public Resource initialValue() {
         return new Resource();
